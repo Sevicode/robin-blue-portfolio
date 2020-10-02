b6a7abd262e55141e2758eb250db3b8a9f803399
Sevda Amini <sevda.amini@hotmail.com>
2020-10-02 22:15:20 +1000

    routes to home page created

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'pages/illustrations'
   get 'pages/contact'
   devise_for :admins
-   #root to: "home#index"
+   root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
