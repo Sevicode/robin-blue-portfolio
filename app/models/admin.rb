97ddea484bdbecb42b2f02e9798480baf3fe9a6f
Sevda Amini <sevda.amini@hotmail.com>
2020-10-04 00:09:18 +1000

    creating the admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
+
+  devise :database_authenticatable, :confirmable, :lockable, :timeoutable, :trackable,
          :recoverable, :trackable, :timeoutable, :lockable, :validatable
+
+  
 end
