f201046761f248d09d69768572e484b6fb7a1f64
Sevda Amini <sevda.amini@hotmail.com>
2020-10-03 21:48:04 +1000

    ruby version changed

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,5 +1,6 @@
 class UploadsController < ApplicationController
   before_action :set_upload, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!
 
   # GET /uploads
   # GET /uploads.json
