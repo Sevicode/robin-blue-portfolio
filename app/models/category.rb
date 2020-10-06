094e04e0c2f61433010de7eea76c45685ef3091f
Sevda Amini <sevda.amini@hotmail.com>
2020-10-06 22:16:29 +1000

    associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+
+    has_many :uploads, foreign_key: "reference_id"
 end
--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,6 +2,6 @@ class Upload < ApplicationRecord
 
     mount_uploader :work, WorkUploader
     # validates :work, file_size: { less_than: 2.megabytes }
-    
+    belongs_to :category, foreign_key: "category_id"
     
 end
